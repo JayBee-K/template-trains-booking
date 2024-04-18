268b87134dc47febe0923c393a6eae062dd8bdc5
Hồng Yến <63199543+nguyenhongyen@users.noreply.github.com>
2024-04-18 15:08:34 +0700

    update code today

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,147 +1,37 @@
 ;(function ($) {
     'use strict';
 
-    const handleSliderHero = function () {
-        if($('#section-hero .swiper').length){
-            new Swiper('#section-hero .swiper', {
-                speed: 1000,
-                slidesPerView: 1,
-                effect: 'fade',
-                navigation: {
-                    nextEl: '#section-hero .button-next',
-                    prevEl: '#section-hero .button-prev',
+
+    const handleSelectInput = function () {
+
+        let selectOption = $('.form-group__select');
+
+        if (selectOption.length) {
+            selectOption.each(function () {
+                let elmSelect = $(this);
+                let inputSelect = elmSelect.find('.input-select');
+                let option = elmSelect.find('.select-group__absolute');
+
+                inputSelect.click(function () {
+                    elmSelect.addClass('show-select');
+                })
+
+                if (option.length) {
+                    let optionElm = option.find('li');
+
+                    optionElm.click(function (e) {
+                        let elm = $(this);
+                        let getTitleOption = elm.data('title');
+
+                        optionElm.removeClass('active');
+                        elm.addClass('active');
+
+                        let a = elmSelect.find('.input-select');
+                        a.val(getTitleOption);
+                        elmSelect.removeClass('show-select');
+                    })
                 }
-            });
-        }
-    }
 
-    const handleSliderCustomers = function () {
-        if($('#section-customers .swiper').length){
-            new Swiper('#section-customers .swiper', {
-                slidesPerView: 1.1,
-                centeredSlides: true,
-                speed: 1000,
-                loop: true,
-                navigation: {
-                    nextEl: '#section-customers .button-next',
-                    prevEl: '#section-customers .button-prev',
-                },
-                breakpoints: {
-                    425: {
-                        spaceBetween: 15,
-                        slidesPerView: 1.2
-                    },
-                    576: {
-                        spaceBetween: 15,
-                        slidesPerView: 1
-                    },
-                    768: {
-                        spaceBetween: 20,
-                        slidesPerView: 2
-                    },
-                    992: {
-                        spaceBetween: 20,
-                        slidesPerView: 2
-                    },
-                    1024: {
-                        slidesPerView: 2,
-                        spaceBetween: 25,
-                    },
-                    1440: {
-                        slidesPerView: 2,
-                        spaceBetween: 40,
-                    },
-
-                }
-            });
-        }
-    }
-
-    const handleSliderReward = function () {
-        if($('#section-reward .swiper').length){
-            new Swiper('#section-reward .swiper', {
-                slidesPerView: 1.3,
-                speed: 1000,
-                spaceBetween: 20,
-                breakpoints: {
-                    375: {
-                        spaceBetween: 10,
-                        slidesPerView: 1.3,
-                    },
-                    425: {
-                        spaceBetween: 10,
-                        slidesPerView: 1.4,
-                    },
-                    576: {
-                        slidesPerView: 2.1,
-                    },
-                    768: {
-                        slidesPerView: 2.5,
-                    },
-                    992: {
-                        slidesPerView: 2.5,
-
-                    },
-                    1024: {
-                        slidesPerView: 3,
-
-                    },
-
-
-                }
-            });
-        }
-    }
-
-    const handleSliderGallery = function () {
-        if($('#section-gallery .swiper').length){
-            new Swiper('#section-gallery .swiper', {
-                slidesPerView: 2.1,
-                spaceBetween: 16,
-                centeredSlides: true,
-                loop: true,
-                speed: 1000,
-                autoplay: {
-                    delay: 6000,
-                    disableOnInteraction: true,
-                },
-                breakpoints: {
-                    425: {
-                        slidesPerView: 2.3
-                    },
-                    576: {
-                        slidesPerView: 3
-                    },
-                    768: {
-                        slidesPerView: 4
-                    },
-                    992: {
-                        slidesPerView: 4
-                    },
-                    1024: {
-                        slidesPerView: 4.3
-                    },
-                    1440: {
-                        slidesPerView: 4.3
-                    },
-
-                }
-            });
-        }
-    }
-
-    const handleCallSidebarService = function(){
-        let btnService = $('#sidebar-service');
-        let closeService = $('#close-service');
-        if(btnService.length){
-            btnService.click(function(){
-                $('body').addClass('show-service');
-            })
-        }
-
-        if(closeService.length){
-            closeService.click(function(){
-                $('body').removeClass('show-service');
             })
         }
     }
@@ -225,14 +115,127 @@
             });
         }
     }
+
+
+    // =============================form checkin=========================
+
+    let handleLoading = function () {
+        $('.loading').show();
+        setTimeout(function () {
+            $('.loading').hide();
+        }, 2000);
+
+    }
+
+    const handleCheckNumberPhone = function () {
+
+        let sectionCheckin = $('#section-checkin');
+        let sectionPhone = $('#section-phone');
+        let sectionLoad = $('#section-load');
+        let sectionCheckList = $('#section-checklist');
+        let sectionBooking = $('#form-booking');
+
+        let btnCheckIn = $('#btn-checkin');
+        if (btnCheckIn.length) {
+            btnCheckIn.click(function (e) {
+                e.preventDefault();
+                handleLoading();
+
+                sectionCheckin.addClass('hide');
+                sectionPhone.removeClass('hide');
+
+                $('#section-phone .btn-previous').click(function(){
+                    sectionCheckin.removeClass('hide');
+                    sectionPhone.addClass('hide');
+                });
+
+            })
+        }
+
+        let btnSubmitPhone = $('#submit-phone');
+        if (btnSubmitPhone.length) {
+            btnSubmitPhone.click(function (e) {
+                e.preventDefault();
+                let elm = $(this);
+
+                let inputNumber = $('#number-input').val();
+                if ((inputNumber !== "") && $.isNumeric(inputNumber)) {
+                    handleLoading();
+                    sectionLoad.removeClass('hide');
+                    sectionPhone.addClass('hide');
+
+                    $('#section-load .btn-previous').click(function(){
+                        sectionLoad.addClass('hide');
+                        sectionPhone.removeClass('hide');
+                    });
+                } else if ((inputNumber !== "") && !$.isNumeric(inputNumber)) {
+                    handleLoading();
+                    sectionCheckList.removeClass('hide');
+                    sectionPhone.addClass('hide');
+                    $('.page-checkin').css({"height": "100% !important","overflow": "visible"});
+
+                    $('#section-checklist .btn-previous').click(function(){
+                        sectionCheckList.addClass('hide');
+                        sectionPhone.removeClass('hide');
+                    });
+
+                } else {
+                    $('#popup-checkphone').modal('show');
+                }
+
+
+            })
+        }
+
+
+        let formCheckChecklist = $('#frm-checkbox');
+        if(formCheckChecklist.length){
+            formCheckChecklist.submit(function(event) {
+                event.preventDefault();
+
+                let isChecked = false;
+                $('#frm-checkbox input[type="checkbox"]').each(function() {
+                    if ($(this).prop('checked')) {
+                        isChecked = true;
+                        return false;
+                    }
+                });
+
+                if (!isChecked) {
+                    $('#popup-checklist').modal('show');
+                } else {
+                    handleLoading();
+                    sectionCheckList.addClass('hide');
+                    sectionBooking.removeClass('hide');
+                    $('.page-checkin').css({"height": "100% !important","overflow": "visible"});
+
+                }
+            });
+        }
+
+        let btnSubmitConfirm = $('#submit-confirm');
+        if (btnSubmitConfirm.length) {
+            btnSubmitConfirm.click(function (e) {
+                e.preventDefault();
+                handleLoading();
+
+                sectionLoad.addClass('hide');
+                sectionBooking.removeClass('hide');
+                $('.page-checkin').css({"height": "100% !important","overflow": "visible"});
+
+            })
+        }
+
+
+
+
+    }
+
     $(function () {
-        handleSliderHero();
-        handleSliderCustomers();
-        handleSliderReward();
-        handleSliderGallery();
-        handleCallSidebarService();
+        handleSelectInput();
         handleSliderCalendar();
         handleSliderClock();
+        handleCheckNumberPhone();
     });
 
 })(jQuery);
